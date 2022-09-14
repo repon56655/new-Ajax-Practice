32640130efb6319370ffe8f945fbfcaf2bdddbb9
repon56655 <md.repon56655@gmail.com>
2022-09-14 23:58:17 +0600

    first commit

---
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Frontend\FrontendModel;
+
+class FrontendController extends Controller
+{
+    public function show(){
+        return view("frontend.dashboard");
+    }
+
+    public function store(Request $request){
+        
+        $data = new FrontendModel;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->message = $request->message;
+
+        return response()->json([
+            'status'=>'success'
+        ]);
+
+       
+
+
+    }
+}
